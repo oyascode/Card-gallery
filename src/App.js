a32bb1a7debbce7d328fd166fd10fca70335b844
Michael John <johnmikeloy@gamail.com>
2021-06-24 05:05:36 +0100

    card gallery

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react"
+import Grid from "@material-ui/core/Grid"
 import './App.css';
+import UserCard from "./UserCard";
+import { Box, makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    width: "80%",
+    maxWidth: "800px",
+    marginRight: "auto",
+    marginLeft: "auto",
+    padding:  theme.spacing(5)
+  },
+  contentWrap: {
+    width: "100%",
+    backgroundColor: "#82ecf",
+    padding: theme.spacing(2),
+    boxShadow: "10px 10px 62px -7px rgba(0,0,0,0.22)"
+  },
+  header: {
+    textAlign: "center",
+    marginBottom: "1em"
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const getUserData = async () => {
+      await fetch("https://reqres.in/api/users?page=2")
+      .then(response => response.json())
+      .then(response => setUsers(response.data))
+    }
+    getUserData();
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box className={classes.container}>
+      <Box className={classes.contentWrap} >
+        <Typography
+        className={classes.header}
+        variant="h5" 
+        component="h1"
+        gutterBottom>
+          Responsive Card Gallery
+        </Typography>
+        <Grid 
+        container 
+        spacing={1}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {users.map(user => 
+            <Grid 
+            item
+            key={user.id} 
+            xs={12} sm={6} md={4} xl={3}
+            >
+              <UserCard 
+              key={user.id} 
+              image={user.avatar} 
+              firstName={user.first_name} 
+              lastName={user.last_name} 
+              email={user.email}
+              />
+            </Grid>
+          )}
+        </Grid>
+      </Box>
+    </Box>
   );
 }
 
---
+++ src/UserCard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+const UserCard = ({image, firstName, lastName, email}) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="user"
+          height="140"
+          image={image}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {`${firstName} ${lastName}`}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {email}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button href={image} size="small" color="primary">
+          Thumbnail pic
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default UserCard;
+
